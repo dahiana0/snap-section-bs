f799f888182fff6d3edafdba262bb9ecdda22c57
dahiana0 <usugakaroldahiana@gmail.com>
2026-02-26 11:25:10 -0500

    cambios clase

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,41 +1,39 @@
+import "../stylessheets/Home.css"
+
 export const Home = () => {
+
     return (
         <>
-            <div className="container text-center">
-                <h1>Row 1</h1>
+            <div className="container">
                 <div className="row">
-                    <div className="col-6 col-md-4 col-lg-3 border">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3 border">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3 border">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3 border">
-                        Column
-                    </div>
-                
-                </div>
+                    <div className="col-6 ">
 
-                <h1>Row 2</h1>
-                <div className="row">
-                    <div className="col-6 border">
-                        Column
-                    </div>
-                    <div className="col-6 border">
-                        Column
-                    </div>
-                    <div className="col-6 border">
-                        Column
-                    </div>
-                    <div className="col-6 border">
-                        Column
+                        <div className="d-flex flex-column justitify-content-center h-100 gap-5">
+
+                            <div >
+                                <h1>MAKE <br /> REMOTE WORK</h1>
+                                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                    Quia labore ut vitae quis impedit nulla quod eos? Ab ea molestias,
+                                    sequi esse ut similique nesciunt veritatis nisi! Eaque, saepe asperiores
+                                </p>
+                                <a href="https://mx.pinterest.com/" className="btn btn-dark">Learn More</a>
+                            </div>
+
+                            <div className="d-flex gap-3 aling-items-center">
+                                <img src="public/logo 3.svg" height="25" alt="" />
+                                <img src="public/mux-logo.png" height="25" alt="" />
+                                <img src="public/wiz.svg" height="25" alt="" />
+                                <img src="public/zephyr_cloud.png"height="25" alt="" />
+                            </div>
+                        </div>
                     </div>
                     
+                    <div className="col-6 ">
+                        <div className="">
+                            <img src="public/img Bost.jpg" className="img-cropped " alt="" />
+                        </div>
+                    </div>
                 </div>
-               
             </div>
         </>
     )
